e03edc99cea5e6ac6a6e2b3bfd74737461da879e
slicingmelon <petre@tutanota.com>
2025-01-22 17:11:36 +0200

    updates

--- internal/engine/rawhttp/requestworkerpool.go
+++ internal/engine/rawhttp/requestworkerpool.go
@@ -85,9 +85,7 @@ func (wp *RequestWorkerPool) processJob(job payload.PayloadJob) *RawHTTPResponse
 	defer wp.httpClient.ReleaseRequest(req)
 	defer wp.httpClient.ReleaseResponse(resp)
 
-	//fmt.Printf("Processing request for URL: %s\n", job.FullURL)
-
-	// Build request
+	// Build HTTP Request
 	if err := wp.BuildRequestTask(req, job); err != nil {
 		if err := wp.errorHandler.HandleError(err, GB403ErrorHandler.ErrorContext{
 			ErrorSource:  []byte("RequestWorkerPool.BuildRequest"),
@@ -118,6 +116,20 @@ func (wp *RequestWorkerPool) BuildRequestTask(req *fasthttp.Request, job payload
 }
 
 // SendRequest sends the HTTP request
+// To remember!
+// ErrNoFreeConns is returned when no free connections available
+// to the given host.
+//
+// Increase the allowed number of connections per host if you
+// see this error.
+//
+// ErrNoFreeConns ErrConnectionClosed may be returned from client methods if the server
+// closes connection before returning the first response byte.
+//
+// If you see this error, then either fix the server by returning
+// 'Connection: close' response header before closing the connection
+// or add 'Connection: close' request header before sending requests
+// to broken server.
 func (wp *RequestWorkerPool) SendRequestTask(req *fasthttp.Request, resp *fasthttp.Response) error {
 	return wp.httpClient.DoRequest(req, resp)
 }
@@ -126,69 +138,3 @@ func (wp *RequestWorkerPool) SendRequestTask(req *fasthttp.Request, resp *fastht
 func (wp *RequestWorkerPool) ProcessResponseTask(resp *fasthttp.Response, job payload.PayloadJob) *RawHTTPResponseDetails {
 	return ProcessHTTPResponse(wp.httpClient, resp, job)
 }
-
-// func main() {
-// 	// Initialize error handler
-// 	errorHandler := GB403ErrorHandler.NewErrorHandler(32)
-// 	httpclientopts := DefaultHTTPClientOptions()
-// 	httpclientopts.ReadBufferSize = 8092      // 8KB
-// 	httpclientopts.WriteBufferSize = 8092     // 8KB
-// 	httpclientopts.MaxResponseBodySize = 4096 // 8KB
-// 	httpclientopts.StreamResponseBody = true
-
-// 	// Create worker pool
-// 	pool := NewWorkerPool(httpclientopts, 10, errorHandler)
-// 	defer pool.Close()
-
-// 	// Generate test URLs
-// 	baseURL := "https://localhost/test/"
-// 	var jobs []payload.PayloadJob
-// 	for i := 1; i <= 200; i++ {
-// 		jobs = append(jobs, payload.PayloadJob{
-// 			Host:         "localhost",
-// 			Method:       "GET",
-// 			FullURL:      fmt.Sprintf("%s%d", baseURL, i),
-// 			PayloadToken: strconv.Itoa(i),
-// 			Headers: []payload.Header{
-// 				{Header: "User-Agent", Value: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36"},
-// 			},
-// 		})
-// 	}
-
-// 	// Start processing
-// 	fmt.Printf("Starting to process %d URLs with %d workers\n", len(jobs), 10)
-// 	startTime := time.Now()
-
-// 	results := pool.ProcessRequests(jobs)
-
-// 	// Process results as they come in
-// 	successCount := 0
-// 	for result := range results {
-// 		if result != nil {
-// 			successCount++
-// 			fmt.Printf("URL: %s - Status: %d - Content Length: %d bytes - Response Bytes: %d bytes\n",
-// 				string(result.URL),
-// 				result.StatusCode,
-// 				result.ContentLength,
-// 				result.ResponseBytes,
-// 			)
-// 			running, waiting := pool.GetCurrentStats()
-// 			fmt.Printf("\rActive Workers: %d | Queued: %d", running, waiting)
-// 		}
-// 	}
-
-// 	// Print statistics
-// 	duration := time.Since(startTime)
-// 	// stats := pool.GetPoolStats()
-
-// 	fmt.Printf("\nExecution Summary:\n")
-// 	fmt.Printf("Total time: %v\n", duration)
-// 	fmt.Printf("Successful requests: %d/%d\n", successCount, len(jobs))
-// 	//fmt.Printf("Running workers: %d\n", stats.RunningWorkers)
-// 	//fmt.Printf("Completed tasks: %d\n", stats.CompletedTasks)
-// 	//fmt.Printf("Failed tasks: %d\n", stats.FailedTasks)
-// 	fmt.Printf("Average time per request: %v\n", duration/time.Duration(len(jobs)))
-
-// 	fmt.Println("")
-// 	errorHandler.PrintErrorStats()
-// }
