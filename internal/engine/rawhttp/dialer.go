bc5069733770b5f10458b880dffd0e0fe45eaa3c
slicingmelon <petre@tutanota.com>
2025-03-04 15:53:38 +0200

    asa

--- internal/engine/rawhttp/dialer.go
+++ internal/engine/rawhttp/dialer.go
@@ -113,50 +113,6 @@ func GetHTTPClientSharedDialer() *fasthttp.TCPDialer {
 // }
 
 // This sets the dialer for the HTTPClient
-// func CreateHTTPClientDialer(timeout time.Duration, proxyURL string) fasthttp.DialFunc {
-
-// 	dialer := GetHTTPClientSharedDialer()
-
-// 	return func(addr string) (net.Conn, error) {
-// 		//GB403Logger.Debug().Msgf("[CreateHTTPClientDialer] Attempting to dial address: %s\n", addr)
-
-// 		// Handle proxy if configured
-// 		if proxyURL != "" {
-// 			proxyDialer := fasthttpproxy.FasthttpHTTPDialerTimeout(proxyURL, timeout)
-// 			conn, err := proxyDialer(addr)
-// 			if err != nil {
-// 				// If it's a whitelisted error, HandleError returns nil
-// 				if handledErr := GB403ErrorHandler.GetErrorHandler().HandleError(err, GB403ErrorHandler.ErrorContext{
-// 					ErrorSource: "Client.proxyDial",
-// 					Host:        addr,
-// 				}); handledErr == nil {
-// 					// This was a whitelisted error, we can ignore it
-// 					return nil, nil
-// 				}
-// 				// Not whitelisted, return just the original error
-// 				return nil, err
-// 			}
-// 			return conn, nil
-// 		}
-
-// 		// No proxy, use our TCPDialer with timeout
-// 		conn, err := dialer.DialDualStackTimeout(addr, timeout)
-// 		if err != nil {
-// 			// If it's a whitelisted error, HandleError returns nil
-// 			if handledErr := GB403ErrorHandler.GetErrorHandler().HandleError(err, GB403ErrorHandler.ErrorContext{
-// 				ErrorSource: "Client.directDial",
-// 				Host:        addr,
-// 			}); handledErr == nil {
-// 				// This was a whitelisted error, we can ignore it
-// 				return nil, nil
-// 			}
-// 			// Not whitelisted, return just the original error
-// 			return nil, err
-// 		}
-// 		return conn, nil
-// 	}
-// }
-
 func CreateHTTPClientDialer(timeout time.Duration, proxyURL string) fasthttp.DialFunc {
 	dialer := GetHTTPClientSharedDialer()
 
